50da48065ffaada213ace1eda6b6bea82616a973
chill31 <89910163+chill31@users.noreply.github.com>
2023-09-21 22:46:23 +0530

    updated to Next.js 13.5

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,26 +6,31 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Chill31 Blog",
-  description: "blog web-app made by Chill31 with the help of Satindar31. It has administrator and user features. Markdown and images supported"
+  description:
+    "blog web-app made by Chill31 with the help of Satindar31. It has administrator and user features. Markdown and images supported",
 };
 
-import {Providers} from "./providers";
-import { ClerkProvider } from '@clerk/nextjs'
-import { dark } from '@clerk/themes';
+import { Providers } from "./providers";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
     <html lang="en">
-      <body className={inter.className}>
-        <Providers>
-          {children}
-          <Toaster />
-        </Providers>
-      </body>
+      <ClerkProvider appearance={{ baseTheme: dark }}>
+        <body className={inter.className}>
+          <Providers>
+            {children}
+            <Toaster />
+          </Providers>
+        </body>
+      </ClerkProvider>
     </html>
-    </ClerkProvider>
   );
 }
