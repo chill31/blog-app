03c1f8c7f8beb7d15d86a143c10385efa6ce98e4
chill31 <89910163+chill31@users.noreply.github.com>
2023-09-21 16:46:52 +0530

    added font

--- components/elements/Title.tsx
+++ components/elements/Title.tsx
@@ -1,4 +1,8 @@
+import { Playfair_Display } from "next/font/google";
 import AuthButtons from "../body/authButtons";
+
+const playfair = Playfair_Display({ subsets: ["latin"] });
+
 
 export default function Title({
   children,
@@ -12,7 +16,7 @@ export default function Title({
   return (
     <>
       <h1
-        className={`text-h1 font-bold max-sm:text-[3rem] max-sm:mt-16 ${
+        className={`${playfair.className} text-h1 font-bold max-sm:text-[3rem] max-sm:mt-16 ${
           className ? className : ""
         }`}
       >
