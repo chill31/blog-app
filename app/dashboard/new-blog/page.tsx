35e34b450e17a616577634772d3e7a19ca68bf37
chill31 <89910163+chill31@users.noreply.github.com>
2023-09-22 21:01:22 +0530

    removed unnecessary function

--- app/dashboard/new-blog/page.tsx
+++ app/dashboard/new-blog/page.tsx
@@ -6,21 +6,6 @@ import NewBlogForm from "@/components/blog/newBlogFom";
 export default async function NewArticle() {
   const user = await currentUser();
 
-  async function createBlog() {
-    const response = await fetch("http://localhost:3000/api/blogs/create", {
-      method: "POST",
-      body: JSON.stringify({
-        content: "ada",
-        title: "ada",
-        authorEmail: user?.emailAddresses[0].emailAddress,
-        shortContent: "ada",
-        isPublic: true,
-        userId: user?.id,
-      }),
-    });
-    const data = await response.json();
-  }
-
   if (!user?.publicMetadata.admin)
     return (
       <Container>
